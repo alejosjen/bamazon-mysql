6ccf47572f91a307e499036429b1a9dd0bfe82e2
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-01-04 14:12:17 -0700

    connection works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,24 @@
 require("dotenv").config();
 var inquirer = require("inquirer");
 var keys = require("./keys.js");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: keys.mySQL.username,
+
+  // Your password
+  password: keys.mySQL.password,
+  database: keys.mySQL.database
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    // createProduct();
+  });
---
+++ bamazonSeeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bible", "Books", 25, 1000),
+				("Cross", "Home & Kitchen", 20, 1000),
+                ("Nativity Set", "Home & Kitchen", 45, 1500),
+                ("White Stringed Lights", "Home & Kitchen", 15, 2500),
+                ("Christmas tree", "Seasonal Decor", 200, 300),
+                ("Christmas tree skirt", "Seasonal Decor", 10, 350),
+                ("Hot Chocolate, 50pack", "Grocery", 15, 400),
+                ("Sparklers", "Home & Kitchen", 5, 2000),
+                ("The Christmas Story", "Movies & TV", 7, 1500),
+                ("It's a Wonderful Life", "Movies & TV", 3, 1500),
+                ("Candles", "Home & Kitchen", 2, 2000),
+                ("Apple Cider", "Grocery", 4, 5000),
+                ("Marshmallows", "Grocery", 2, 10000),
+                ("Hershey bars", "Grocery", 3, 10000),
+                ("Graham crackers", "Grocery", 3, 10000);
+                
+SELECT * FROM products;
