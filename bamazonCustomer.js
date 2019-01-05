6688bf371debe6ad269dd0cf27491f48cecfd06c
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-01-05 12:17:24 -0700

    Made connection.connect into a large function with mini ones inside. Added checkStock function to see if stock is available or not.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,75 +4,105 @@ var keys = require("./keys.js");
 var mysql = require("mysql");
 const Table = require('cli-table');
 
-var connection = mysql.createConnection({
-    host: keys.mySQL.host,
-    port: keys.mySQL.port,
-    user: keys.mySQL.username,
-    password: keys.mySQL.password,
-    database: keys.mySQL.database
-});
+// var connection = mysql.createConnection({
+//     host: keys.mySQL.host,
+//     port: keys.mySQL.port,
+//     user: keys.mySQL.username,
+//     password: keys.mySQL.password,
+//     database: keys.mySQL.database
+// });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
-});
 
-function readProducts() {
-    var query = "SELECT * FROM products";
-    connection.query(query, function (error, response) {
-        if (error) throw error;
-        const table = new Table({
-            head: ['ID', 'Product', 'Department', 'Price', 'Stock']
-            , colWidths: [5, 25, 18, 8, 9]
+    function readProducts() {
+        var query = "SELECT * FROM products";
+        connection.query(query, function (error, response) {
+            if (error) throw error;
+            //Create table of data
+            const table = new Table({
+                head: ['ID', 'Product', 'Price']
+                , colWidths: [5, 35, 8]
+            });
+            for (var i = 0; i < response.length; i++) {
+                var itemUniqueID = response[i].item_id;
+                var productName = response[i].product_name;
+                var department = response[i].department_name;
+                var price = response[i].price;
+                var stockQuantity = response[i].stock_quantity;
+                table.push(
+                    [itemUniqueID,
+                        productName,
+                        ("$") + price
+                    ]);
+            };
+            //Display data
+            console.log(table.toString());
+            //Call function to begin ordering
+            promptOrder();
         });
-
-        for (var i = 0; i < response.length; i++) {
-        table.push(
-            [response[i].item_id, 
-            response[i].product_name, 
-            response[i].department_name, 
-            response[i].price, 
-            response[i].stock_quantity]
-        );
-        };
-        console.log(table.toString());
-        promptOrderID();
-    });
-};
-
-function promptOrderID() {
-    inquirer.prompt([
-        {
-            name: "order",
-            message: "Please, type in the ID# of your order."
-        }
-    ]).then(function (itemID, error) {
-        var orderID = itemID.order;
-        var string = orderID.toString();
-        var number = Number.parseFloat(string);
-            if (Number.isNaN(number)|| orderID === "" || orderID === false || orderID === null || orderID === undefined) {
-                console.log("Please retry by entering an ID number.");
-                promptOrderID();
-            } else {
-                var customerOrder = orderID;
-                promptHowMany();
+    };
+    function promptOrder() {
+        inquirer.prompt([
+            {
+                name: "order",
+                message: "Please, type in the ID# of your order."
+            },
+            {
+                name: "amount",
+                message: "How many would you like to order?"
             }
-    })
-}
+        ]).then(function (answer, error) {
+            if (error) throw error;
 
-function promptHowMany() {
-    inquirer.prompt([
-        {
-            name: "amount",
-            message: "How many would you like to order?"
-        }
-    ]).then(function (answer, error) {
-        if (answer.amount === "" || answer.amount === NaN || answer.amount === false || answer.amount === null) {
-            console.log("Please retry by entering a number.");
-            promptHowMany();
-        } else {
+            //Input item request
+            var orderID = answer.order;
+            var string = orderID.toString();
+            var number = Number.parseFloat(string);
+
+            //Input-validation for item request
+            if (Number.isNaN(number) || orderID === "" || orderID === false || orderID === null || orderID === undefined) {
+                console.log("Please retry by entering an ID number.");
+                promptOrder();
+            }
+
+            //Input order amount
             var amountOrdered = answer.amount;
-        }
-    }); connection.end();
-};
+            var stringIt = amountOrdered.toString();
+            var isNumber = Number.parseFloat(stringIt);
+
+            //Input-validation for order amount
+            if (Number.isNaN(isNumber) || amountOrdered === 0 || amountOrdered === "" || amountOrdered === false || amountOrdered === null || amountOrdered === undefined) {
+                console.log("Please retry by entering a number or amount greater than 0.");
+                promptOrder();
+            }
+
+            function checkStock() {
+                var query = "SELECT * FROM products WHERE item_ID=" + orderID;
+                connection.query(query, function (error, response) {
+                    if (error) throw error;
+                    // var stock = response;
+                    for (var j = 0; j < response.length; j++) {
+                        var customerSelection = response[j].item_id;
+                        var customerProductName = response[j].product_name;
+                        var customerDepartment = response[j].department_name;
+                        var customerPrice = response[j].price;
+                        var customerStockAvailable = response[j].stock_quantity;
+
+                        if (amountOrdered <= customerStockAvailable) {
+                            console.log("Your requested amount: " + amountOrdered);
+                            console.log("In Stock");
+                        } else {
+                            console.log("Insufficient quantity!")
+                            console.log("Amount available: " + customerStockAvailable);
+                        };
+                    }
+                })
+            };
+            checkStock();
+            connection.end();
+        })
+    };
+});
